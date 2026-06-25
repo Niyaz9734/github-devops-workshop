6a2f31fb7b657d54e58935eda3fafc970043defe
Zee-Niyaz <kniyazul093@gmail.com>
2026-06-26 00:18:33 +0530

    again

--- demo.py
+++ demo.py
@@ -1,3 +1,7 @@
 name = input("Enter your name: ")
 print(f"Hello, {name}!")
 print(f"Did you know? 1 + 2 + 3 + 4 + 5 = {sum(range(1, 6))}")
+
+name = input("What is your name? ")
+print("Hello, " + name + "!")
+
